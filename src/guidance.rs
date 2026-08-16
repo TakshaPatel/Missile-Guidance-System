59bc11538474e8f3561c429be545362acf6ad5a2
Taksha Patel <takshapatel3@gmail.com>
2026-08-16 10:58:07 -0400

    default guidence

--- src/guidance.rs
+++ src/guidance.rs
@@ -18,10 +18,6 @@ pub fn proportional_navigation(state: &EngagementState, nav_gain: f64) -> Vec3 {
 
     let los_rate = los.cross(rel_vel) / (range * range);
     let unit_missile = state.missile_vel / missile_speed;
-    let unit_los = los / range;
-    //let closing_speed = -rel_vel.dot(unit_los);
-    let closing_speed = (-rel_vel.dot(unit_los)).max(0.0);
- 
-    //let closing_speed = rel_vel.norm();
+    let closing_speed = rel_vel.norm();
     los_rate.cross(unit_missile) * (nav_gain * closing_speed)
 }
